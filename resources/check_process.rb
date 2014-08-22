a1a2d54c8c4bc328bf318d8aac1e895f0e320aa6
Peter Bell <peter_bell@htc.com>
2014-08-22 11:22:49 -0700

    remove requirement for start and stop program scripts

--- resources/check_process.rb
+++ resources/check_process.rb
@@ -5,9 +5,9 @@ attribute :service_name, :name_attribute => true
 attribute :type, :default => "pid", :regex => /^(pid|matching)$/, :required => true
 attribute :pidfile, :kind_of => String
 attribute :regex, :default => "", :kind_of => String
-attribute :start_program, :kind_of => String, :required => true
+attribute :start_program, :kind_of => String, :required => false
 attribute :start_timeout, :kind_of => Fixnum, :required => false
-attribute :stop_program,  :kind_of => String, :required => true
+attribute :stop_program,  :kind_of => String, :required => false
 attribute :stop_timeout,  :kind_of => Fixnum, :required => false
 attribute :restart_program,  :kind_of => String, :required => false
 attribute :restart_timeout,  :kind_of => Fixnum, :required => false
